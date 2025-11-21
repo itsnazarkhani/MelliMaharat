e2e95fa580e98c1238995452094b131246ccc5d7
Michelle <michellejorjany@gmail.com>
2025-11-20 23:22:14 -1200

    Added View_SelectedLessonsInformation To Database by changing Migration itself manually

---
+++ DataForge/DataForge.Dal/Migrations/20251121111258_Add View_SelectedLessonsInformation Itself.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataForge.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddView_SelectedLessonsInformationItself : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql
+                (
+                    @"
+                        CREATE VIEW [dbo].[View_SelectedLessonsInformation] AS
+                            SELECT 
+                                Students.FullName as 'StudentName',
+                                Lessons.Name as 'LessonName',
+                                Masters.FullName as 'MasterName',
+                                Presentations.StartTime as 'LessonStartTime',
+                                Presentations.EndTime as 'LessonEndTime',
+                                Lessons.Unit as 'LessonUnit',
+                                Selections.EducationYear as 'EducationYear',
+                                Selections.Score as 'Score'
+                            FROM Students
+                                INNER JOIN Selections on Selections.StudentId = Students.Id
+                                INNER JOIN Presentations on Presentations.Id = Selections.PresentationId
+                                INNER JOIN Masters on Masters.Id = Presentations.MasterId
+                                INNER JOIN Lessons on Lessons.Id = Presentations.LessonId 
+                    "
+                );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW [dbo].[View_SelectedLessonsInformation]");
+        }
+    }
+}
