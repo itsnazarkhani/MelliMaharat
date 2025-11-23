0245634d81db4d7bef5ec22c0e1a3a6dfae28654
Michelle <michellejorjany@gmail.com>
2025-11-23 00:35:30 -1200

    Add Many Styles for Custom Controls

--- DataForge/DataForge.Wpf/Resources/UserControls/CustomTextBox.xaml.cs
+++ DataForge/DataForge.Wpf/Resources/UserControls/CustomTextBox.xaml.cs
@@ -6,18 +6,17 @@
 public partial class CustomTextBox : UserControl
 {
     public static readonly DependencyProperty InputTextProperty;
+
     public string InputText
     {
         get { return (string)GetValue(InputTextProperty); }
         set { SetValue(InputTextProperty, value); }
     }
 
-
     public CustomTextBox() =>InitializeComponent();
     static CustomTextBox() =>
         InputTextProperty = DependencyProperty.Register(nameof(InputText), typeof(string), typeof(CustomTextBox), new FrameworkPropertyMetadata(defaultValue: "", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
-
-    private void txtInput_TextChanged(object sender, TextChangedEventArgs e) =>
-        lblError.Visibility = string.IsNullOrWhiteSpace(lblError.Text) ? Visibility.Hidden : Visibility.Visible;
+    //private void txtInput_TextChanged(object sender, TextChangedEventArgs e) =>
+    //    lblError.Visibility = string.IsNullOrWhiteSpace(lblError.Text) ? Visibility.Hidden : Visibility.Visible;
 }
