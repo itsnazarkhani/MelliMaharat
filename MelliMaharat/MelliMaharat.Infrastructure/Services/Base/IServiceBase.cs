6e9fed16636d33cf2b41693d245ddc0870a77d5a
itsnazarkhani <m.nazarkhani1380@gmail.com>
2025-11-26 11:16:39 +0330

    feat: add base interface for services

---
+++ MelliMaharat/MelliMaharat.Infrastructure/Services/Base/IServiceBase.cs
@@ -0,0 +1,16 @@
+﻿using MelliMaharat.Dal.Repos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MelliMaharat.Infrastructure.Services.Base
+{
+    public interface IServiceBase
+    {
+        StudentRepo Students { get; set; }
+        PresentationRepo Presentation { get; set; }
+        SelectionRepo Selections { get; set; }
+        MasterRepo Masters { get; set; }
+        LessonRepo Lessons { get; set; }
+    }
+}
