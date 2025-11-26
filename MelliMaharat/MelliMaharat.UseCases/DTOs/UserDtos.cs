d683a06312e4c94e3135558628a18a69b3431faa
itsnazarkhani <m.nazarkhani1380@gmail.com>
2025-11-26 10:37:20 +0330

    feat: add user dtos

---
+++ MelliMaharat/MelliMaharat.UseCases/DTOs/UserDtos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MelliMaharat.UseCases.DTOs
+{
+    public record class LoginDto (string Username, string Password);
+
+    public record class UserDto
+    (
+        Guid Id,
+        string FirstName,
+        string LastName,
+        int Age,
+        string NationalCode,
+        string PhoneNumber,
+        string Email,
+        string Username,
+        bool IsAdmin,
+        string Role
+    );
+}
