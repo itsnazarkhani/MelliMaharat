9952e20bad5e0f97c1d17506de20c29dd87bf0b3
itsnazarkhani <m.nazarkhani1380@gmail.com>
2025-11-26 11:24:08 +0330

    feat: implement base service

--- MelliMaharat/MelliMaharat.Infrastructure/Services/Base/IServiceBase.cs
+++ MelliMaharat/MelliMaharat.Infrastructure/Services/Base/IServiceBase.cs
@@ -7,10 +7,10 @@ namespace MelliMaharat.Infrastructure.Services.Base
 {
     public interface IServiceBase
     {
-        StudentRepo Students { get; set; }
-        PresentationRepo Presentation { get; set; }
-        SelectionRepo Selections { get; set; }
-        MasterRepo Masters { get; set; }
-        LessonRepo Lessons { get; set; }
+        StudentRepo Students { get; init; }
+        PresentationRepo Presentations { get; init; }
+        SelectionRepo Selections { get; init; }
+        MasterRepo Masters { get; init; }
+        LessonRepo Lessons { get; init; }
     }
 }
---
+++ MelliMaharat/MelliMaharat.Infrastructure/Services/Base/ServiceBase.cs
@@ -0,0 +1,30 @@
+﻿using MelliMaharat.Dal.Repos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MelliMaharat.Infrastructure.Services.Base
+{
+    public class ServiceBase : IServiceBase
+    {
+        public StudentRepo Students { get; init; }
+        public PresentationRepo Presentations { get; init; }
+        public SelectionRepo Selections { get; init; }
+        public MasterRepo Masters { get; init; }
+        public LessonRepo Lessons { get; init; }
+
+        public ServiceBase(
+            StudentRepo studentRepo,
+            PresentationRepo presentationRepo,
+            SelectionRepo selectionRepo,
+            MasterRepo masterRepo,
+            LessonRepo lessonRepo)
+        {
+            Students = studentRepo;
+            Presentations = presentationRepo;
+            Selections = selectionRepo;
+            Masters = masterRepo;
+            Lessons = lessonRepo;
+        }
+    }
+}
