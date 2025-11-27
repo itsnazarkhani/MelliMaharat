edd36c7002fd51f1e66ad577ddc9f5059e523226
itsnazarkhani <m.nazarkhani1380@gmail.com>
2025-11-27 19:33:04 +0330

    refactor: change directory name of repositories

--- MelliMaharat/MelliMaharat.Dal/Repository/Interfaces/IRepo.cs
+++ MelliMaharat/MelliMaharat.Dal/Repository/Interfaces/IRepo.cs
@@ -5,10 +5,10 @@ public interface IRepo<T> : IViewRepo<T> where T : BaseEntity
     int Add(T entity);
     int AddRange(IEnumerable<T> entities);
     int Remove(T entity);
-    int Remove(int id);
+    int Remove(Guid id);
     int RemoveAll();
     int RemoveRange(IEnumerable<T> entities);
     int Update(T entity);
     int UpdateRange(IEnumerable<T> entities);
-    T GetById(int id);
+    T GetById(Guid id);
 }
