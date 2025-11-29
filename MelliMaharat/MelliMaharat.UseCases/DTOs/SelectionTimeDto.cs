4fa1ce549a70d5be8d2a495afe903a78195bf6c2
itsnazarkhani <m.nazarkhani1380@gmail.com>
2025-11-29 19:23:27 +0330

    feat: add SelectionDtos

---
+++ MelliMaharat/MelliMaharat.UseCases/DTOs/SelectionTimeDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MelliMaharat.UseCases.DTOs
+{
+    public record class NewSelectionTimeDto(
+            DateTime SelectionStart,
+            DateTime SelectionEnd
+        );
+
+    public record class SelectionTimeDto(
+            Guid Id,
+            DateTime SelectionStart,
+            DateTime SelectionEnd
+        );
+}
