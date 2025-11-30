7c728b66403325e14616a251f4375f20f122a586
itsnazarkhani <m.nazarkhani1380@gmail.com>
2025-11-30 08:37:07 +0330

    Revert "fix: comment out @mmigueljocode written repos for master and student"
    This reverts commit b16730323822e7c05d40876837fb3ecc6eeba9f0.

--- MelliMaharat/MelliMaharat.Dal/Repos/MasterRepo.cs
+++ MelliMaharat/MelliMaharat.Dal/Repos/MasterRepo.cs
@@ -1,26 +1,26 @@
-﻿//namespace MelliMaharat.Dal.Repos;
+﻿namespace MelliMaharat.Dal.Repos;
 
-//public class MasterRepo : Repo<Master>, IUser
-//{
-//    public MasterRepo() : base() { }
-//    public MasterRepo(ApplicationDbContext context) : base(context) { }
+public class MasterRepo : Repo<Master>, IUser
+{
+    public MasterRepo() : base() { }
+    public MasterRepo(ApplicationDbContext context) : base(context) { }
 
-//    public bool IsAdmin(string username, string password)
-//    {
-//        return _table.FirstOrDefault(x => x.PersonInformation.Username == username && x.PersonInformation.Password == password)
-//                     .PersonInformation
-//                     .IsAdmin;
-//    }
+    public bool IsAdmin(string username, string password)
+    {
+        return _table.FirstOrDefault(x => x.PersonInformation.Username == username && x.PersonInformation.Password == password)
+                     .PersonInformation
+                     .IsAdmin;
+    }
 
-//    public bool IsPasswordMatch(string username, string password)
-//    {
-//        var user = _table.FirstOrDefault(x => x.PersonInformation.Username == username);
-//        return user.PersonInformation.Password == password;
-//    }
+    public bool IsPasswordMatch(string username, string password)
+    {
+        var user = _table.FirstOrDefault(x => x.PersonInformation.Username == username);
+        return user.PersonInformation.Password == password;
+    }
 
-//    public bool IsUserExist(string username)
-//    {
-//        return _table.Where(x => x.PersonInformation.Username == username)
-//                     .Any();
-//    }
-//}
+    public bool IsUserExist(string username)
+    {
+        return _table.Where(x => x.PersonInformation.Username == username)
+                     .Any();
+    }
+}
