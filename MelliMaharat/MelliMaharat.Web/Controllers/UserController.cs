106fcb47a34a5cba7118df5a29653a93a40eebc0
itsnazarkhani <m.nazarkhani1380@gmail.com>
2025-11-30 21:50:34 +0330

    feat: initial user controller with dashboard action method

---
+++ MelliMaharat/MelliMaharat.Web/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MelliMaharat.Models.Enums;
+using MelliMaharat.Web.Filters;
+
+namespace MelliMaharat.Web.Controllers
+{
+    [AuthorizeByRole(UserRoles.Student, UserRoles.Master, UserRoles.Admin)]
+    public class UserController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
