282da97c150904f2cfc21e20d08e534d50344a58
itsnazarkhani <m.nazarkhani1380@gmail.com>
2025-11-30 21:10:13 +0330

    feat: add student controller with views

---
+++ MelliMaharat/MelliMaharat.Web/Controllers/StudentController.cs
@@ -0,0 +1,13 @@
+﻿using MelliMaharat.Models.Enums;
+using MelliMaharat.Web.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+public class StudentController : Controller
+{
+    [AuthorizeByRole(UserRoles.Student)]
+    public IActionResult TermLessons() => View();
+
+    public IActionResult SelectUnits() => View();
+    public IActionResult Scores() => View();
+    public IActionResult Attendance() => View();
+}
