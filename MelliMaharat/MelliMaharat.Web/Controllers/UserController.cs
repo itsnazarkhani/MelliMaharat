ce77a950523e04fd0937ce5531a0c0fa2df21e7f
itsnazarkhani <m.nazarkhani1380@gmail.com>
2025-11-30 23:58:58 +0330

    feat: dashboard page shows user details

--- MelliMaharat/MelliMaharat.Web/Controllers/UserController.cs
+++ MelliMaharat/MelliMaharat.Web/Controllers/UserController.cs
@@ -1,15 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MelliMaharat.Models.Enums;
+using MelliMaharat.Dal.UnitOfWork;
 using MelliMaharat.Web.Filters;
+using MelliMaharat.Models.Enums;
+using MelliMaharat.Web.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace MelliMaharat.Web.Controllers
 {
     [AuthorizeByRole(UserRoles.Student, UserRoles.Master, UserRoles.Admin)]
     public class UserController : Controller
     {
-        public IActionResult Dashboard()
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserController(IUnitOfWork unitOfWork)
         {
-            return View();
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            // Get logged-in user's ID from claims
+            var userIdStr = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (!Guid.TryParse(userIdStr, out var userId))
+            {
+                return Unauthorized();
+            }
+
+            // Fetch user with PersonInformation
+            var user = await _unitOfWork.Users
+                .GetAll()
+                .Include(u => u.PersonInformation)
+                .FirstOrDefaultAsync(u => u.Id == userId);
+
+            if (user == null)
+                return NotFound();
+
+            // Map to ViewModel
+            var model = new UserDashboardViewModel
+            {
+                FullName = $"{user.PersonInformation.FirstName} {user.PersonInformation.LastName}",
+                PhoneNumber = user.PersonInformation.PhoneNumber,
+                AvatarId = user.AvatarId,
+                Role = user.Role.ToString()
+            };
+
+            return View(model);
         }
     }
 }
---
+++ MelliMaharat/MelliMaharat.Web/ViewModels/UserDashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MelliMaharat.Web.ViewModels
+{
+    public class UserDashboardViewModel
+    {
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public Guid AvatarId { get; set; }
+        public string Role { get; set; }
+    }
+}
