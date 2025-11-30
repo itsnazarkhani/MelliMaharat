ae20dc093a4ce6560f33a3b6d307af3fad42ee52
itsnazarkhani <m.nazarkhani1380@gmail.com>
2025-11-30 21:09:05 +0330

    feat: add admin controller with views

---
+++ MelliMaharat/MelliMaharat.Web/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using MelliMaharat.Models.Enums;
+using MelliMaharat.Web.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+public class AdminController : Controller
+{
+    [AuthorizeByRole(UserRoles.Admin)]
+    public IActionResult Lessons() => View();
+
+    public IActionResult Masters() => View();
+    public IActionResult Students() => View();
+    public IActionResult Presentations() => View();
+    public IActionResult CreateEvent() => View();
+}
