4cb754fa2932fdef0e8cbaa6bddec2bac8d376c9
itsnazarkhani <m.nazarkhani1380@gmail.com>
2025-11-30 22:05:38 +0330

    feat: handle lessons list and lesson addition functinality in admin controller

--- MelliMaharat/MelliMaharat.Web/Controllers/AdminController.cs
+++ MelliMaharat/MelliMaharat.Web/Controllers/AdminController.cs
@@ -1,14 +1,55 @@
-﻿using MelliMaharat.Models.Enums;
+﻿using MelliMaharat.Dal.UnitOfWork;
+using MelliMaharat.Models;
+using MelliMaharat.Models.Enums;
 using MelliMaharat.Web.Filters;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-public class AdminController : Controller
+namespace MelliMaharat.Web.Controllers
 {
     [AuthorizeByRole(UserRoles.Admin)]
-    public IActionResult Lessons() => View();
+    public class AdminController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
 
-    public IActionResult Masters() => View();
-    public IActionResult Students() => View();
-    public IActionResult Presentations() => View();
-    public IActionResult CreateEvent() => View();
+        public AdminController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IActionResult> Lessons()
+        {
+            var lessons = await _unitOfWork.Lessons
+                .GetAll()
+                .OrderBy(l => l.Name)
+                .ToListAsync();
+
+            return View(lessons);
+        }
+
+        public IActionResult AddLesson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddLesson(Lesson lesson)
+        {
+            if (!ModelState.IsValid)
+                return View(lesson);
+
+            await _unitOfWork.Lessons.AddAsync(lesson);
+            await _unitOfWork.CommitChangesAsync();
+
+            return RedirectToAction("Lessons");
+        }
+
+
+
+
+        public IActionResult Masters() => View();
+        public IActionResult Students() => View();
+        public IActionResult Presentations() => View();
+        public IActionResult CreateEvent() => View();
+    }
 }
