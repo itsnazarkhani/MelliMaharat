21d864eae4d04375783fef4d36f313ab0dc9f868
itsnazarkhani <m.nazarkhani1380@gmail.com>
2025-11-30 21:09:55 +0330

    feat: add master controller with views

---
+++ MelliMaharat/MelliMaharat.Web/Controllers/MasterController.cs
@@ -0,0 +1,9 @@
+﻿using MelliMaharat.Models.Enums;
+using MelliMaharat.Web.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+public class MasterController : Controller
+{
+    [AuthorizeByRole(UserRoles.Master)]
+    public IActionResult PresentedLessons() => View();
+}
