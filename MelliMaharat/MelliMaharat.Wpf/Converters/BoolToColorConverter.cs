5d096adae82149d4b843014211e7d35898bdcedf
Michelle <michellejorjany@gmail.com>
2025-12-02 00:17:16 -1200

    Changed Color Of Background, MenuBottons Now RadioButtons, EntitiesList for Manager.MastersPage is Completed

--- MelliMaharat/MelliMaharat.Wpf/Converters/BoolToColorConverter.cs
+++ MelliMaharat/MelliMaharat.Wpf/Converters/BoolToColorConverter.cs
@@ -4,8 +4,10 @@ public class BoolToColorConverter : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
+        Color color = (Color)ColorConverter.ConvertFromString("#1B263B");
+
         if (value is bool result)
-            return result ? Red : MediumAquamarine;
+            return result ? Red : color;
         else
             return Transparent;
     }
--- MelliMaharat/MelliMaharat.Wpf/Windows/Pages/Manager/MastersPage.xaml.cs
+++ MelliMaharat/MelliMaharat.Wpf/Windows/Pages/Manager/MastersPage.xaml.cs
@@ -5,5 +5,35 @@
 /// </summary>
 public partial class MastersPage : Page
 {
-    public MastersPage() => InitializeComponent();
+    List<Models.Master>? Masters { get; set; } = [];
+    List<Person> Persons { get; set; } = [];
+    Models.Master? Master { get; set; }
+    ICollectionView? View { get; set; } = default;
+    string Graduation { get; set; } = "Doctorate";
+    public MastersPage()
+    {
+        new MasterRepo().GetAll().ToList().ForEach(x => Masters?.Add(x));
+        Masters.ForEach(x => Persons.Add(x.PersonInformation));
+        InitializeComponent();
+        //DataContext = Masters;
+        Master = Masters?.First();
+        string firstName = Master!.PersonInformation.FirstName;
+        string fullName = Master!.PersonInformation.FullName;
+        Graduation = Master.Graduation;
+        txtFirstName.InputText = firstName;
+        this.DataContext = Graduation;
+        listbox.DataContext = Persons;
+
+        View = CollectionViewSource.GetDefaultView(Persons);
+        View.SortDescriptions.Clear();
+        View.SortDescriptions.Add(new SortDescription(nameof(Person.FirstName), ListSortDirection.Ascending));
+        listbox.ItemsSource = View;
+    }
+
+    private async void Page_Loaded(object sender, RoutedEventArgs e)
+    {
+        await Task.Delay(5000);
+        View!.SortDescriptions.Clear();
+        View.SortDescriptions.Add(new SortDescription(nameof(Person.Age), ListSortDirection.Ascending));
+    }
 }
