fc99d024a7d0282bd571c5fc008f470c6cf3508f
Michelle <michellejorjany@gmail.com>
2025-12-04 11:23:41 -1200

    Cleaning Things Up

--- MelliMaharat/MelliMaharat.Wpf/Windows/Pages/Master/ProfilePage.xaml.cs
+++ MelliMaharat/MelliMaharat.Wpf/Windows/Pages/Master/ProfilePage.xaml.cs
@@ -1,26 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿namespace MelliMaharat.Wpf.Windows.Pages.Master;
 
-namespace MelliMaharat.Wpf.Windows.Pages.Master
+/// <summary>
+/// Interaction logic for ProfilePage.xaml
+/// </summary>
+public partial class ProfilePage : Page
 {
-    /// <summary>
-    /// Interaction logic for ProfilePage.xaml
-    /// </summary>
-    public partial class ProfilePage : Page
-    {
-        public ProfilePage()
-        {
-            InitializeComponent();
-        }
-    }
+    public ProfilePage() => InitializeComponent();
 }
