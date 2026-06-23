5e2a314813c519b29bf21ca813f3fc45b4b16a3b
Mohammad Nazarkhani <itsnazarkhani@gmail.com>
2026-06-23 22:06:28 +0330

    feat: add core user domain entities

---
+++ src/MelliMaharat.Domain/Entities/Instructor.cs
@@ -0,0 +1,14 @@
+﻿using MelliMaharat.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MelliMaharat.Domain.Entities
+{
+    public class Instructor : BaseEntity
+    {
+        public string EmployeeNumber { get; set; } = null!;
+
+        public string UserId { get; set; } = null!;
+    }
+}
